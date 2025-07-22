684c97b40b72c7ef0f827e08689de22ecf42af5d
falbnrocha <falbnrocha@gmail.com>
2025-07-22 11:50:24 +0100

    update

--- page_analyse.py
+++ page_analyse.py
@@ -30,28 +30,27 @@ def page_analyse():
     # Select type of progress curves
     with make_expanders("**Type of progress curves:**"):
         # st.markdown("**type of progress curves:**")
-        analysis_mode = st.radio("Options", ('in concentration units'
-                                             , 'raw data - calibration curve needed:'))
+        analysis_mode = st.radio("Options", ('in concentration units',
+                                             'raw data - known calibration curve',
+                                             'raw data - unknown background/calibration curve'))
         if analysis_mode == 'in concentration units':
             slope = 1
             intercept = 0
+        elif analysis_mode == 'raw data - known calibration curve':
+            slope = st.number_input("Slope [signal/concentration]", format="%2.2f", value = 1.)
+            intercept = st.number_input("Background [signal]", format="%2.2f", value = 0.)
         else:
-            slope = st.number_input("Slope [concentration/signal]", format="%2.2f", value = 1.)
-            intercept = st.number_input("Intercept [concentration]", format="%2.2f", value = 0.)
+            slope = st.number_input("Slope [signal/concentration]", format="%2.2f", value = 1.)
+            intercept = 0
 
-    # analysis_mode = st.sidebar.radio('**What type of progress curves do you have?**', ('Mass-based', 'Size-based'))   
-    # st.sidebar.markdown("#")
-  
     st.markdown("<h2 style='text-align: center;'> Enzyme Kinetic Analysis </h2>", 
                 unsafe_allow_html=True)
         
     st.info("""
-                - Upload progress curves measured at different (>4) substrate concentrations
-                - Find Michaelis-Menten parameters
+                - Upload progress curves measured at different substrate concentrations
                 - Estimate  initial reaction rates
+                - Find Michaelis-Menten parameters
                 """)
-    # st.markdown("#")
-    
     
     # Figure display properties expander
     with make_expanders("Select Figure Mode:"):
@@ -178,7 +177,7 @@ def page_analyse():
             else:
                 dfi = df.iloc[:, [0,n+1]].astype(float) 
             t, P = extract_curves(dfi)
-            P = P*slope + intercept
+            P = (P - intercept)/slope
             mint.extend([min(t)])
             maxt.extend([max(t)])
             minP.extend([min(P)])
@@ -210,6 +209,10 @@ def page_analyse():
             P = Si - odeint(dS_dt, Si, t, args=(V, K))
             return P.flatten()
         
+        def substrate_ode2(t, V, K, Si):
+            P = Si - odeint(dS_dt, Si, t, args=(V, K))
+            return P.flatten()
+        
         fig1 = plt.figure(facecolor=backc)
         # fig2 = plt.figure(facecolor=backc)
         Km_int = []
@@ -222,21 +225,35 @@ def page_analyse():
                 else:
                     dfi = df.iloc[:, [0,n+1]].astype(float) 
                 t, P = extract_curves(dfi)
-                P = P*slope + intercept
+                P = (P - intercept)/slope
                 if SvstFlag == Ncurves:
                     P = S0[n] - P
                 dt = t-list(t)[0]
                 dP = P -list(P)[0]
-                ig = np.asarray([max(S0)/10000, max(np.diff(dP)/np.diff(dt))])
-                bds =  ([0, ig[1]], [np.inf, np.inf])
-                Si = S0[n] - list(P)[0]
-                parameters, covariance = curve_fit(substrate_ode, dt, dP, 
+                
+                ig = np.asarray([max(S0)/10, max(np.diff(dP)/np.diff(dt)), S0[n]])
+                bds =  ([0, ig[1], min(dP)], [np.inf, np.inf, np.inf])
+                parameters, covariance = curve_fit(substrate_ode2, dt, dP, 
                                   p0=ig, bounds=bds, maxfev=10000) #xtol=1e-20*tmax, ftol=1e-20*ymax, 
+                Si = parameters[2]
                 
                 
+                if analysis_mode != 'raw data - unknown background/calibration curve':    
+                    ig = np.asarray([max(S0)/10000, max(np.diff(dP)/np.diff(dt))])
+                    bds =  ([0, ig[1]], [np.inf, np.inf])
+                    
+                    if Si > 0.9 * (S0[n] - list(P)[0]): # no enzyme inactivation
+                        Si = S0[n] - list(P)[0]
+                        S0_int.extend([S0[n]])
+                    else:
+                        S0_int.extend([Si])
+                        
+                    parameters, covariance = curve_fit(substrate_ode, dt, dP, 
+                                      p0=ig, bounds=bds, maxfev=10000) #xtol=1e-20*tmax, ftol=1e-20*ymax, 
+                    
                 Km_int.extend([parameters[1]])
-                Vmax_int.extend([parameters[0]])
-                S0_int.extend([Si])
+                Vmax_int.extend([parameters[0]]) 
+                
                 p_xfit = np.linspace(0, max(dt))
                 p_yfit = substrate_ode(p_xfit, parameters[0], parameters[1])
                 if plot_mode == 'Dark Mode':
@@ -267,89 +284,10 @@ def page_analyse():
         pointnumber_flag = all(i < 3 for i in pointnumber)
     
         if not(pointnumber_flag):
-            
-            percentile95 = []
-            index95 = []
-            intfilt_all = []
-            for n in range(Ncurves):
-                if template == 1:
-                    dfi = df.iloc[:, [2*n,2*n+1]].astype(float)
-                else:
-                    dfi = df.iloc[:, [0,n+1]].astype(float) 
-                t, P = extract_curves(dfi)
-                P = P*slope + intercept
-                if SvstFlag == Ncurves:
-                    P = S0[n] - P
-                t = t-list(t)[0]
-                P = P -list(P)[0]
-                percentile95.extend([np.percentile(P, 95)])
-                index95.extend([(np.abs(P - percentile95[n])).argmin()])
-                intfilt_all.extend([round(index95[n]/nosample)]);
-            
-            # Definition of largest interval for strong filter application
-            intfilthalf = round(max(intfilt_all)/2) if round(max(intfilt_all)/2) >0 else 1 ;
-            
-            Pfiltered = []
-            tfiltered = []
-            for n in range(Ncurves):
-                if template == 1:
-                    dfi = df.iloc[:, [2*n,2*n+1]].astype(float)
-                else:
-                    dfi = df.iloc[:, [0,n+1]].astype(float) 
-                t, P = extract_curves(dfi)
-                P = P*slope + intercept
-                if SvstFlag == Ncurves:
-                    P = S0[n] - P
-                t = t-list(t)[0]
-                P = P -list(P)[0]
-                filtereddata = []
-                filteredtime = []
-                for q in range(intfilthalf, (pointnumber[n]-intfilthalf+2)):
-                    filtereddata.extend([np.mean( P[(q - intfilthalf):(q + intfilthalf)] )])  
-                    filteredtime.extend([np.mean( t[(q - intfilthalf):(q + intfilthalf)] )])
-                Pfiltered.append(np.transpose(filtereddata))
-                tfiltered.append(np.transpose(filteredtime))
-            
-            # Sampling x Points, Product-wise
-            Psampled = []
-            tsampled = []
-            UniqueFlag = 0
-            for n in range(Ncurves):
-                tempP = Pfiltered[n]
-                tempt = tfiltered[n]
-                productsamples = np.linspace(min(tempP) , max(tempP), nosample )
-                sampleP = []
-                samplet = []
-                for q in range(nosample):
-                    closestindex = (np.abs(tempP - productsamples[q])).argmin()
-                    sampleP.extend([tempP[closestindex]])
-                    samplet.extend([tempt[closestindex]])
-                # check if arrays are unique
-                if len(sampleP) > len(set(sampleP)):
-                    UniqueFlag = 1
-                Psampled.append(np.transpose(sampleP))
-                tsampled.append(np.transpose(samplet))
-                
+            a = 1               
         # Less than 3 timepoints/curve or non-unique  
         else:
-            Psampled = []
-            tsampled = []
-            for n in range(Ncurves):
-                tempP = Pfiltered[n]
-                tempt = tfiltered[n]
-                Psampled.append(np.transpose(tempP))
-                tsampled.append(np.transpose(tempt))
             st.warning('\n Interferenzy analysis not performed. Number of timepoints < 3.\n')
-
-        # Non-unique arrays
-        if UniqueFlag == 1:
-            Psampled = []
-            tsampled = []
-            for n in range(Ncurves):
-                tempP = Pfiltered[n]
-                tempt = tfiltered[n]
-                Psampled.append(np.transpose(tempP))
-                tsampled.append(np.transpose(tempt))
 
         # Fit MM curve
         def MM(x, param0, param1):
@@ -364,7 +302,7 @@ def page_analyse():
         
         v0 = []
         for n in range(Ncurves):
-            v0.extend([S0[n]*Vmax_int[n] / (S0[n] + Km_int[n])])
+            v0.extend([S0_int[n]*Vmax_int[n] / (S0_int[n] + Km_int[n])])
         v0_df = pd.DataFrame({'S0': S0,
                    'v0': v0})
             
@@ -390,35 +328,7 @@ def page_analyse():
         plt.ylabel('v0')
         plt.legend(loc="upper left")
         st.pyplot(fig2)
-        
-        # Interferenzy Analysis------------------------------------------------
-        FinalP = S0_int
-        X = []
-        Y = []
-        for n in range(Ncurves):
-            arg_log = 1 - (Psampled[n][1] - Psampled[n][0])/(FinalP[n] - Psampled[n][0])
-            if arg_log > 0:
-                linearX = -np.log(arg_log) / (tsampled[n][1] - tsampled[n][0])
-                linearY = (Psampled[n][1] - Psampled[n][0]) / (tsampled[n][1] - tsampled[n][0])
-                X.extend([linearX])
-                Y.append(np.transpose(linearY))
-            else:
-                st.warning('\n\nError during Interferenzy linearization in at least 1 curve.')
-
-        Y2 = []
-        for n in range(Ncurves):
-            Y2.append(Vmax_int[n] - Km_int[n]*X[n])
-   
-    
-        # Reporting
-        st.info('Simplified Interferenzy Analysis')
-        try:
-            # Pearson correlation
-            corr, p_value = pearsonr(Y, Y2)
-            st.write(f"\n\nPearson correlation: {corr:.3f}, p-value: {p_value:.3e}")
-        except:
-            st.write('\n\nInterferenzy validation not possible.') 
-            
+                  
         def count_sig_figs(value):
             """
             Count significant figures in a number.
@@ -503,10 +413,11 @@ def page_analyse():
             
             return format_string.format(mean_rounded, sd_rounded)    
         
+        
+        st.info('\n\nInitial Rates:\n')
+        st.dataframe(v0_df, hide_index=True)
         st.info('Michaelis-Menten Parameters:')
         st.write(f"\nKm ± SD: {format_mean_sd(Km, sigma_ab[0])}")
         st.write(f"\nV ± SD: {format_mean_sd(Vmax, sigma_ab[1])}")
-        st.info('\n\nInitial Rates:\n')
-        st.dataframe(v0_df, hide_index=True)
         
         
